f8ac68a99463c737f25904cc562db5dad181558e
Renee Messersmith <morganrmessersmith@gmail.com>
2024-06-15 19:30:34 -0400

    Add final answers

--- final_practice.rb
+++ final_practice.rb
@@ -16,35 +16,62 @@ puts last_laugh
 
 
 # 1: Write a method named "greeting" that returns a string with a generic greeting. 
+def greeting
+  "Hello there!"
+end
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
+puts greeting
+
+# What is the return value of your method? The value returned is "Hello there!"
+# How many arguments did you pass your method? 1 argument
 
 
 # 2: Write a method named "custom_greeting" that returns a greeting WITH a specific name.
+def custom_greeting(name)
+  "Hello, #{name}!"
+end
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+puts custom_greeting("Phoenix")
+
+# What is the return value of your method? The returnn value is "Hello, Phoenix!"
+# How many arguments did you pass your method? 1 argument
+# What data type was your argument(s)? the argument was a string with variable interpolation.
 
 
 # 3: Write a method named "greet_person" that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
+def greet_person(first, middle, last)
+  "Hello #{first} #{middle} #{last}!"
+end
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+puts greet_person("Morgan", "Renee", "Messersmith")
+
+# What is the return value of your method? The return value is "Hello Morgan Renee Messersmith!".
+# How many arguments did you pass your method? This method passed 3 arguments.
+# What data type was your argument(s)? Strings.
 
 
 # 4: Write a method named "square" that takes in one integer, and returns the square of that integer.
 # Bonus: Print a sentence that interpolates the return value of your square method. You should not have to update the method itself to do this.
+def square(number)
+  "The square of #{number} is #{number * number}."
+end
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+puts square(8)
+
+# What is the return value of your method? "The square of 8 is 64." 
+# How many arguments did you pass your method? This method passed though 1 arguments.
+# What data type was your argument(s)? Integers.
 
 
 # 5: Write a method named "check_stock" that satisfies the following interaction pattern:
 # Hint: You will only write one check_stock method that checks the quantity and then prints the corresponding statement.
+def check_stock(num, ingredient)
+ if num >= 4
+  print"#{ingredient} is stocked"
+ else num <= 3
+  print "#{ingredient} - running low"
+ end
+end
 
 check_stock(4, "Coffee");
 # => "Coffee is stocked"
--- self_eval.md
+++ self_eval.md
@@ -1,4 +1,6 @@
 # Self-Evaluation
 
 - Reflect on your learning and work today. What are you most proud of?
+I was struggling with wrapping my brain around methods. I got a little lost during the lecture, so I am glad I was able to take the time to really understand it. I am feeling much more confident in the topic.
 - What is the power of using methods in our applications?
+Methods are great tools for taking an input and returning an output whenever needed. It gives us the ability to make code re-usable.
